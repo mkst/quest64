8688386c35843e7df20cd0ae54c13c15030fbc4b
Mallos31 <mallos31@LAPTOP-QTT77J20.localdomain>
2022-11-14 16:54:46 -0500

    match functions to load the 3 overlays

--- include/functions.h
+++ include/functions.h
@@ -3,3 +3,4 @@ s32 func_8003195C(s8, u8);
 s32 func_80031BB0(s32);
 s32 osContInit(OSMesgQueue*, u8*, OSContStatus*);
 
+void func_80024260(u8* arg0, void* arg1, u32 arg2);                 /* extern */
--- src/8650.c
+++ src/8650.c
@@ -15,7 +15,29 @@
 #pragma GLOBAL_ASM("asm/nonmatchings/8650/func_800086E4.s")
 
 #pragma GLOBAL_ASM("asm/nonmatchings/8650/func_80008948.s")
+//Matching pending struct updates
+/*
+void func_80008948(s32 arg0) {
+    
+    unk8948s* temp_v1;
 
+    temp_v1 = &D_8007BD30[arg0];
+    
+    if (temp_v1->unk80->unk4 == 2) {
+        temp_v1->unk0 = 0;
+        temp_v1->unk2 = (s16) (func_80022FD0(0x1EU) + 0x1E);
+        return;
+    }
+    if (temp_v1->unk80->unk5 & 2) {
+        temp_v1->unk0 = 0;
+        temp_v1->unk2 = (s16) temp_v1->unk80->unk10->unk2;
+        return;
+    }
+    temp_v1->unk0 = 3;
+    temp_v1->unk2 = 0xF;
+    func_8001D8B0(temp_v1->unk14, 1, 1, 1, 1, (u16) 1);
+}
+*/
 #pragma GLOBAL_ASM("asm/nonmatchings/8650/func_80008A00.s")
 
 #pragma GLOBAL_ASM("asm/nonmatchings/8650/func_80008AD8.s")
